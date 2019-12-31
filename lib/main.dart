e6ebd92498c8c15f69bdb30f99a0bc285043ee1d
Edgar Ceron <edgar@MacBook-Air-de-Edgar-241.local>
2019-12-30 20:26:38 -0600

    Se crea bottomNavigationBar con cupertino para poder poner la barra con efecto transparente modo ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'content_trips.dart';
+import 'content_trips_cupertino.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: ContentTrips()
+      home: ContentTripsCupertino()
     );
   }
 }
