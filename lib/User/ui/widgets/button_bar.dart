11dc463c3113005243109f305df1313a1aca9acc
Edgar Ceron <edgar@MacBook-Air-de-Edgar-241.local>
2020-01-01 22:15:12 -0600

    Se implementa funcionalidad para poder cerrar sesion en la app

--- lib/User/ui/widgets/button_bar.dart
+++ lib/User/ui/widgets/button_bar.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:trip_app/User/bloc/bloc_user.dart';
 import 'circle_button.dart';
 
 class ButtonsBar extends StatelessWidget {
+  UserBloc userBloc;
 
   @override
   Widget build(BuildContext context) {
+    userBloc = BlocProvider.of(context);
     return Padding(
         padding: EdgeInsets.symmetric(
             horizontal: 0.0,
@@ -12,9 +16,20 @@ class ButtonsBar extends StatelessWidget {
         ),
         child: Row(
           children: <Widget>[
-            CircleButton(true, Icons.edit, 20.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(true, Icons.exit_to_app, 20.0, Color.fromRGBO(255, 255, 255, 0.6))
+            CircleButton(
+              ()=> {},
+              true, Icons.edit, 20.0, Color.fromRGBO(255, 255, 255, 1)
+            ),
+            CircleButton(
+              ()=> {},
+              false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1)
+            ),
+            CircleButton(
+              ()=> {
+                userBloc.signOut()
+              },
+              true, Icons.exit_to_app, 20.0, Color.fromRGBO(255, 255, 255, 0.6)
+            )
           ],
         )
     );
